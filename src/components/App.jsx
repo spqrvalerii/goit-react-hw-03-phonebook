4b00ca1806ccb7d296d25acaa8e3444dc04b81a8
spqrvalerii <1982elcondorpasa@gmail.com>
2023-04-04 08:25:08 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,9 +13,7 @@ export class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    filter: '',
-    name: '',
-    number: '',
+    filter: '',    
   };
 
   componentDidMount() {
